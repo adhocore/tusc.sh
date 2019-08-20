6711bf05a69d847d241affe026c49268c041f23b
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-08-20 18:27:32 +0700

    init: tusc.sh with hashbang

---
+++ tusc.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# TUS client protocol implementation for bash.
+#
+# Author:
+#   Jitendra Adhikari <jiten.adhikary@gmail.com>
+#
+# Be sure to check readme doc at https://github.com/adhocore/bash-tus
+#
