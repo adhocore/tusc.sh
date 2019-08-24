a4d74f8ea42da51a15ec05663b48e22c5633fa35
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-08-24 08:18:46 +0700

    feat: add tusc --update

--- tusc.sh
+++ tusc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# TUS client protocol implementation for bash.
+# TUS client 1.0.0 protocol implementation for bash.
 #
 # Author:
 #   Jitendra Adhikari <jiten.adhikary@gmail.com>
@@ -26,6 +26,20 @@ comment() { line "$1" 30 1; }
 # show version
 version() { echo v0.1.1; }
 
+# update tusc
+update()
+{
+  NEWVER=`curl -sSL https://raw.githubusercontent.com/adhocore/tusc.sh/master/VERSION`
+  if [ "v$NEWVER" == "$(version)" ]; then
+    ok "Already latest version"
+    return 0
+  fi
+
+  info "Updating $TUSC ..."
+  curl -sSLo ${FULL} https://raw.githubusercontent.com/adhocore/tusc.sh/master/tusc.sh
+  ok "  Done [${NEWVER}]"
+}
+
 # show usage
 usage()
 {
@@ -44,9 +58,11 @@ usage()
     $(info "-f --file")      $(comment "The file to upload.")
     $(info "-h --help")      $(comment "Show help information and usage.")
     $(info "-H --host")      $(comment "The tus-server host where file is uploaded.")
+    $(info "-u --update")    $(comment "Update tusc to latest version.")
 
   $(ok Examples:)
     $TUSC --help                           # shows this help
+    $TUSC --update                         # updates itself
     $TUSC --version                        # prints current version of itself
     $TUSC    0:1080    ww.mp4              # uploads ww.mp4 to http://0.0.0.0:1080/files/
     $TUSC -H 0:1080 -f ww.mp4              # same as above
@@ -114,8 +130,6 @@ on-exit()
   fi
 }
 
-trap on-exit EXIT
-
 # argv parsing
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -124,6 +138,7 @@ while [[ $# -gt 0 ]]; do
     -f | --file) FILE="$2"; shift 2 ;;
     -h | --help | help) usage $1; exit 0 ;;
     -H | --host) HOST="$2"; shift 2 ;;
+    -u | --update) update; exit 0 ;;
          --version | version) version; exit 0 ;;
     *) if [[ $HOST ]]; then
         if [[ $FILE ]]; then SUMALGO="${SUMALGO:-$1}"; else FILE="$1"; fi
@@ -131,6 +146,8 @@ while [[ $# -gt 0 ]]; do
       shift ;;
   esac
 done
+
+trap on-exit EXIT
 
 [[ $HOST ]] || error "--host required" 1
 [[ $FILE ]] || error "--file required" 1
